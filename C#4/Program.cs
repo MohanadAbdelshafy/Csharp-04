841aba8de9bdce34df8576bba764c07fda2bfe42
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-09-05 22:04:13 +0300

    Question2

--- C#4/Program.cs
+++ C#4/Program.cs
@@ -8,6 +8,10 @@
             //double[] prices = { 25.5,40.0,33.75 };
             //Console.WriteLine(prices[1]);
             #endregion
+            #region Question2
+            //int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+            //Console.WriteLine(shelfCopies[1, 0]);
+            #endregion
         }
     }
 }
