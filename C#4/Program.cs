8744dd190415a3de46d235b00ee8a604ca63ded5
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-09-05 22:07:48 +0300

    Question 3

--- C#4/Program.cs
+++ C#4/Program.cs
@@ -2,6 +2,12 @@
 {
     internal class Program
     {
+        #region Question 3 function
+        //static void PrintWelcomeMessage()
+        //{
+        //    Console.WriteLine("Welcome to the Library!");
+        //}
+        #endregion
         static void Main(string[] args)
         {
             #region Question 1
@@ -12,6 +18,10 @@
             //int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
             //Console.WriteLine(shelfCopies[1, 0]);
             #endregion
+            #region Question 3 Fuction call
+            //PrintWelcomeMessage();
+            #endregion
+
         }
     }
 }
